a0c7a0cbb0545b6bcb2f86d88f0e300a8da471ba
zf49 <zwan686@aucklanduni.ac.nz>
2021-03-15 18:13:09 +0800

    测试test

--- src/gitClone/LearnGit/Test01.java
+++ src/gitClone/LearnGit/Test01.java
@@ -1,9 +1,7 @@
 package gitClone.LearnGit;
 
-public class CloneFromGit {
+public class Test01 {
     public static void main(String[] args) {
-        System.out.println("Clone From Git");
+        System.out.println("测试git");
     }
 }
-
-
