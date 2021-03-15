eaa36fafebcf13c9ffb321ed8e4794e89db2b5f1
zf49 <zwan686@aucklanduni.ac.nz>
2021-03-15 18:48:38 +0800

    qweq

--- src/gitClone/LearnGit/asdadasdasd.java
+++ src/gitClone/LearnGit/asdadasdasd.java
@@ -1,12 +1,13 @@
 package gitClone.LearnGit;
 
-public class Asdadasdasd {
+class Asdadasdasd {
     public static void main(String[] args) {
         System.out.println("1231231231231");
         System.out.println("7777777777");
 
         System.out.println("666666666666");
         System.out.println("666666666666");
+        System.out.println("123");
         System.out.println("666666666666");
         System.out.println("666666666666");
 
