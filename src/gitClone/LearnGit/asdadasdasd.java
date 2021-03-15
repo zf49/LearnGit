da0af2b38af8bde6c22aeda3a3d1f45886bd9e85
zf49 <zwan686@aucklanduni.ac.nz>
2021-03-15 18:41:49 +0800

    123123

--- src/gitClone/LearnGit/asdadasdasd.java
+++ src/gitClone/LearnGit/asdadasdasd.java
@@ -4,7 +4,14 @@ public class Asdadasdasd {
     public static void main(String[] args) {
         System.out.println("1231231231231");
         System.out.println("1231231231231");
-        System.out.println("1231231231231");
-        System.out.println("1231231231231");
+
+        System.out.println("666666666666");
+        System.out.println("666666666666");
+        System.out.println("666666666666");
+        System.out.println("666666666666");
+
+
+
+
     }
 }
